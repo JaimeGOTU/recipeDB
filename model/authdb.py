aafeac42f71936731b9c0a79622ac9dd4e90a819
Nate Coolidge <nathanael.coolidge@ontariotechu.net>
2024-04-01 20:19:06 -0400

    Delete is now working fully

--- model/authdb.py
+++ model/authdb.py
@@ -7,7 +7,7 @@ db = SQLAlchemy(os.getenv("DATABASE_URL", "mysql+pymysql://root:4iqX0rBR2IdLx2ud
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(25), unique=True)
-    password = db.Column(db.String(512))
+    email = db.Column(db.String(1000), unique=True)
+    password = db.Column(db.String(1000))
     name = db.Column(db.String(1000))
     picture = db.Column(db.String(1000))
